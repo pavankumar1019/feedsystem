024f2f9701936e34bbee04ea491c39fb32106d60
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-22 12:11:32 +0530

    a
    a

---
+++ admin/fetch_class.php
@@ -0,0 +1,35 @@
+<?php
+include('database_connection.php');
+$query = "SELECT * FROM class ORDER BY id DESC";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+$total_rows = $statement->rowCount();
+
+$output = '
+<option id="hell" selected>Open this select menu</option>
+';
+if($total_rows > 0)
+{
+	foreach($result as $row)
+	{
+		$output .= '
+        <option id="hell" value="'.$row["id"].'">'.$row["course"].'-'.$row["sem"].'</option>
+		';
+	}
+}
+else
+{
+	$output .= '
+    <option selected>No record</option>
+	';
+}
+$output .= '</table></div>';
+
+echo $output;
+
+?>
---
+++ admin/staff/fetch.php
@@ -0,0 +1,59 @@
+<?php
+include('../database_connection.php');
+
+$query = "SELECT staff.id,staff.name,staff.email,staff.phone,staff.sub, class.course,class.sem
+FROM staff
+INNER JOIN class ON staff.class_id = class.id;";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+$total_rows = $statement->rowCount();
+
+$output = '
+<div class="table-responsive">
+	<table class="table table-bordered table-striped">
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Email</th>
+			<th>phone</th>
+			<th>sub</th>
+			<th>Class</th>
+			<th>Delete</th>
+		</tr>
+';
+
+if($total_rows > 0)
+{
+	foreach($result as $row)
+	{
+		$output .= '
+		<tr>
+			<td>'.$row["id"].'</td>
+			<td>'.$row["name"].'</td>
+			<td>'.$row["email"].'</td>
+			<td>'.$row["phone"].'</td>
+			<td>'.$row["sub"].'</td>
+			<td>'.$row["course"].'-sem('.$row["sem"].')</td>
+			<td><button type="button" name="delete" id="'.$row["id"].'" class="btn btn-danger btn-xs delete">Delete</button></td>
+		</tr>
+		';
+	}
+}
+else
+{
+	$output .= '
+	<tr>
+		<td colspan="4">No Data Found</td>
+	</tr>
+	';
+}
+$output .= '</table></div>';
+
+echo $output;
+
+?>
